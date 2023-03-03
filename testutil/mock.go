016eab4427082a5144fedd1df5640eea7cba3ac4
Conor Patrick <cpatrick@jumptrading.com>
2023-03-03 10:44:08 -0600

    fix mocked client for balances

--- testutil/mock.go
+++ testutil/mock.go
@@ -12,6 +12,8 @@ type MockedClient struct {
 	mock.Mock
 }
 
+var _ xc.ClientBalance = &MockedClient{}
+
 // FetchTxInput fetches tx input, mocked
 func (m *MockedClient) FetchTxInput(ctx context.Context, from xc.Address, to xc.Address) (xc.TxInput, error) {
 	args := m.Called(ctx, from, to)
@@ -40,4 +42,14 @@ func (m *MockedClient) FetchBalance(ctx context.Context, address xc.Address) (xc
 func (m *MockedClient) FetchNativeBalance(ctx context.Context, address xc.Address) (xc.AmountBlockchain, error) {
 	args := m.Called(ctx, address)
 	return args.Get(0).(xc.AmountBlockchain), args.Error(1)
+}
+
+func (m *MockedClient) FetchBalanceForAsset(ctx context.Context, address xc.Address, assetCfg xc.AssetConfig) (xc.AmountBlockchain, error) {
+	args := m.Called(ctx, address)
+	return args.Get(0).(xc.AmountBlockchain), args.Error(1)
+}
+
+func (m *MockedClient) UpdateAsset(assetCfg xc.AssetConfig) error {
+	args := m.Called(assetCfg)
+	return args.Error(1)
 }
