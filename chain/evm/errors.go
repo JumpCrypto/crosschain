6565446a8f34c8648bb963aa997218f3ea8e17a6
Emanuele Cesena <ecesena@jumptrading.com>
2023-05-31 11:25:03 -0700

    Fix Polygon "transaction underpriced" (#55)
    Co-authored-by: Emanuele Cesena <ecesena@jumpcrypto.com>

--- chain/evm/errors.go
+++ chain/evm/errors.go
@@ -8,16 +8,18 @@ import (
 
 func CheckError(err error) xc.ClientError {
 	msg := strings.ToLower(err.Error())
-	if strings.Contains(msg, "transaction underpriced") {
-		return xc.TransactionFailure
-	}
 	if strings.Contains(msg, "insufficient funds for gas * price + value") {
 		return xc.NoBalanceForGas
 	}
 	if strings.Contains(msg, "insufficient funds for transfer") {
 		return xc.NoBalance
 	}
-	if strings.Contains(msg, "response body closed") ||
+	// Polygon seems to return "transaction underpriced" but still forwarding the tx the chain,
+	// that eventually accepts the tx.
+	// By returning a network error, we expect the tx to be retried and eventually found on chain
+	// as "already known".
+	if strings.Contains(msg, "transaction underpriced") ||
+		strings.Contains(msg, "response body closed") ||
 		strings.Contains(msg, "not found") ||
 		strings.Contains(msg, "eof") {
 		return xc.NetworkError
